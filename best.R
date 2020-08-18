2e022ba1891291a9ef3b2a04d3b70c8086ce8f51
Shalom P <rahulnalluru007@gmail.com>
2020-08-18 11:45:04 +0530

    now it is complete assignment project

--- best.R
+++ best.R
@@ -1,19 +1,28 @@
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-best<-function(state,outcome){
-  a<-unlist(strsplit(outcome,"\\ "))
-  for(i in 1:length(a)){
-    b<-NULL
-    d<-unlist(strsplit(a[i],""))
-    d[1]<-toupper(d[1])
-    for(j in 1:length(d)){
-      if(is.null(b)){
-        b<-paste(d[j])
-      }
-      else{
-        b<-paste(b,d[j],sep = "")
-      }
-    }
-    a[i]<-b
+best<-function(State,outcom){
+  y<-unique(outcome$State)
+  if(outcom == "heart attack"){
+    a<-tapply(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,outcome$State,min)
+    b<-a[names(a)== State]
+    n<-outcome$Hospital.Name[(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == b) & (outcome$State == State)]
   }
-  a
+  else if(outcom == "heart failure"){
+    a<-tapply(as.numeric(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),outcome$State,min,na.rm = TRUE)
+    b<-a[names(a)== State]
+    n<-outcome$Hospital.Name[(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure == b) & (outcome$State == State)]
+  }
+  else if(outcom == "pneumonia"){
+    a<-tapply(as.numeric(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),outcome$State,min,na.rm = TRUE)
+    b<-a[names(a)==State]
+    n<-outcome$Hospital.Name[(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == b) & (outcome$State == State)]
+  }
+  for(i in 0){
+    if(!(State %in% y)){
+      n<-paste('Error in best(',State,', ',outcom,') : invalid state')
+    }
+    else if((outcom != "heart attack") & (outcom != "heart failure") & (outcom != "pneumonia")){
+      n<-paste("Error in best(",State,", ",outcom,") : invalid outcome")
+    }
+  }
+  n
 }
