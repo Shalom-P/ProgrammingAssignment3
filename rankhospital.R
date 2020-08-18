e45d370c7824f649e90d5ae64f1e91a27cfb6fbd
Shalom P <rahulnalluru007@gmail.com>
2020-08-18 16:21:16 +0530

    this is the final review every requirement is met and also it works like a beauty

--- rankhospital.R
+++ rankhospital.R
@@ -1,6 +1,7 @@
 outcome <- read.csv("outcome-of-care-measures.csv")
 rankhospital<-function(state,outcom,num = "best"){
-  if(outcom == "heart failure"){
+  y<-unique(outcome$State)
+  if((outcom == "heart failure") & (state %in% y)){
     if(num == "best"){
       num<-1
     }
@@ -22,6 +23,25 @@ rankhospital<-function(state,outcom,num = "best"){
       num<- length(o)
     }
     n<-outcome$Hospital.Name[(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == o[num]) & (outcome$State==state)]
+  }else if((sum(outcom != c("heart attack","heart failure", "pneumonia"))) | !(state %in% y)){
+    if(sum(outcom != c("heart attack","heart failure", "pneumonia")) & (state %in% y)){
+      n<-"invalid outcome"
+    }else if(!(state %in% y) & sum(outcom == c("heart attack","heart failure", "pneumonia"))){
+      n<-"invalid state"
+    }else {
+      n<-"invalid outcome and invalid state"
+    }
+  }else if(outcom == "pneumonia"){
+    if(num == "best"){
+      num<-1
+    }
+    a<-tapply(as.numeric(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),outcome$State,sort)
+    b<-a[names(a) == state]
+    o<-unlist(b)
+    if(num == "worst"){
+      num<-length(o) 
+    }
+    n<-outcome$Hospital.Name[(outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == o[num]) & (outcome$State == state)]
   }
   n[1]
 }
